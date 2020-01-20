45eb7ecce4f49c1064f3bff708600aa65d390288
AdibHoque <46087857+AdibHoque@users.noreply.github.com>
2020-01-20 22:58:20 +0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -44,16 +44,5 @@ module.exports = class {
         let giveawaysOptions = { updateCountdownEvery: 15000, ignoreIfHasPermission: [ "ADMINISTRATOR" ], storage: require("path").resolve()+"/giveaways.json" };
         giveaways.launch(client, giveawaysOptions);
 
-        // Update the game every 20s
-        const status = require("../config.js").status,
-        version = require("../package.json").version;
-        let i = 0;
-        setInterval(function(){
-            let toDisplay = status[parseInt(i, 10)].name.replace("{serversCount}", client.guilds.size)+" | v"+version;
-            client.user.setActivity(toDisplay, {type: status[parseInt(i, 10)].type});
-            if(status[parseInt(i+1, 10)]) i++
-            else i = 0;
-        }, 20000); // Every 20 seconds
-
     }
 }  
